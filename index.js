6770b91d9197f7e948af1616d6f45ad8c789a500
CsSebas01 <escobarsebastian1230@gmail.com>
2024-10-15 23:53:26 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let btnCart = document.querySelector('.container-icon')
-let containerCartProducts = document.querySelector('.container-cart-products')
-
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle
-    ('hidden-cart')
-})
-
