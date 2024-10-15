071435f8b4cd6223a52e63ef1b291c9ada181828
CsSebas01 <escobarsebastian1230@gmail.com>
2024-10-15 23:49:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const btnCart = document.querySelector('.container-icon')
-const containerCartProducts = document.querySelector('.container-cart-products')
+let btnCart = document.querySelector('.container-icon')
+let containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle
